6cb4ab7b5e6c7061c07a6dc95f15a8d42ae1f2d3
Hui Yang <hui.yang.3737@gmail.com>
2023-09-17 09:02:11 -0400

    deleted demo-qa

--- demo-qa/src/test/java/automation/utilities/UtilsTester.java
+++
@@ -1,9 +0,0 @@
-package automation.utilities;
-
-public class UtilsTester {
-
-	public static void main(String[] args) {
-		System.out.println(String.format("document.body.style.transform='scale(%d)';", 0.8));
-	}
-
-}
