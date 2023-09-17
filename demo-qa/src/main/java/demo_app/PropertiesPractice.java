50a49b673f294b32bbc116fd540e3e9c3bd6b930
Hui Yang <hui.yang.3737@gmail.com>
2023-09-17 12:32:10 -0400

    updated PropertiesPractice

--- demo-qa/src/main/java/demo_app/PropertiesPractice.java
+++ demo-qa/src/main/java/demo_app/PropertiesPractice.java
@@ -9,7 +9,8 @@ public class PropertiesPractice {
 		for (String propertyName : sp.stringPropertyNames()) {
 			System.out.println(propertyName);
 		}
-		System.out.println("\nJava: " + System.getProperty("java.version"));
+		System.out.println("\nTotal: " + sp.stringPropertyNames().size());
+		System.out.println("Java: " + System.getProperty("java.version"));
 		System.out.println(System.getProperty("os.name") + ", " + System.getProperty("os.version"));
 		System.out.println(System.getProperty("user.name"));
 		System.out.println(System.getProperty("user.dir"));
