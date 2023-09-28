3f91cf36f930148e0ec294f506a1901c33aec6aa
Hui Yang <hui.yang.3737@gmail.com>
2023-09-28 13:36:39 -0400

    added PracticeWebTest

--- demo-practice-junit-5/src/test/java/demo_tests/ui/SeleniumFirstTest.java
+++ demo-practice-junit-5/src/test/java/demo_tests/ui/SeleniumFirstTest.java
@@ -1,8 +1,12 @@
 package demo_tests.ui;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utilities.DriverFactory;
 import utilities.SeleniumServices;
@@ -13,8 +17,10 @@ public class SeleniumFirstTest {
 	void openDMV() {
 		WebDriver driver = DriverFactory.getChrome();
 		SeleniumServices ss = new SeleniumServices();
-		driver.get("https://www.dmv.virginia.gov/");
-		driver.findElement(By.xpath("//nav//a[@href='/locations']")).click();
+		driver.get("https://www.usps.com/");
+		driver.findElement(By.linkText("Shop")).click();
+		new WebDriverWait(driver, Duration.ofSeconds(15)).until(ExpectedConditions.urlContains("/store"));
+		driver.findElement(By.linkText("Stamps")).click();
 		ss.pause(3);
 
 		System.out.println("Yay!");
