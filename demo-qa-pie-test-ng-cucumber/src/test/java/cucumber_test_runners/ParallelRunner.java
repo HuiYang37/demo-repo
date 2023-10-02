305effe1b8e1552b88381e01b18011ffd770c384
Hui Yang <hui.yang.3737@gmail.com>
2023-10-02 16:08:31 -0400

    added ParallelRunner

---
+++ demo-qa-pie-test-ng-cucumber/src/test/java/cucumber_test_runners/ParallelRunner.java
@@ -0,0 +1,19 @@
+package cucumber_test_runners;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "./src/test/resources/features", glue = { "step_definitions", "cucumber_hooks" }, plugin = {
+		"pretty", "html:target/html_reports/cucumber.html",
+		"json:target/json_reports/cucumber.json" }, dryRun = false, tags = "@ui")
+public class ParallelRunner extends AbstractTestNGCucumberTests {
+
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+
+}
