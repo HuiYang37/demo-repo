fb1680bc2f089867ac7fedf944c2aa7785516bf6
Hui Yang <hui.yang.3737@gmail.com>
2023-10-19 19:04:49 -0400

    implemented singleton design

--- demo-qa-junit-5/src/test/java/utilities/DriverManager.java
+++ demo-qa-junit-5/src/test/java/utilities/DriverManager.java
@@ -1,33 +1,50 @@
 package utilities;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
 
-	private static ThreadLocal<DriverManager> threadLocalDriverManager;
-	private static WebDriver driver;
+	private static ThreadLocal<WebDriver> threadLocalDriver;
+	private static String browserName;
+	private static boolean headless;
 
 	private DriverManager() {
 	}
 
-	public static void initManager(String browserName) {
-		if (threadLocalDriverManager == null)
-			threadLocalDriverManager = new ThreadLocal<DriverManager>();
-		if (threadLocalDriverManager.get() == null)
-			threadLocalDriverManager.set(new DriverManager());
-		if (driver == null)
-			driver = SeleniumService.findDriver(browserName);
-	}
-
 	public static WebDriver getInstance() {
-		return driver;
+		if (threadLocalDriver == null)
+			threadLocalDriver = new ThreadLocal<>();
+		if (threadLocalDriver.get() == null) {
+			WebDriver driver;
+			if (headless)
+				driver = DriverFactory.getHeadlessChrome();
+			else if (browserName == null)
+				driver = DriverFactory.getChrome();
+			else
+				driver = SeleniumService.findDriver(browserName);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+			driver.manage().window().maximize();
+			threadLocalDriver.set(driver);
+		}
+		return threadLocalDriver.get();
 	}
 
-	public static void tearDownDriver() {
-		if (driver != null)
-			driver.quit();
-		if (threadLocalDriverManager != null)
-			threadLocalDriverManager = null;
+	public static void reset() {
+		if (threadLocalDriver.get() != null)
+			threadLocalDriver.get().quit();
+		threadLocalDriver.set(null);
+		headless = false;
+		browserName = null;
+	}
+
+	public static void setHeadless(boolean headless) {
+		DriverManager.headless = headless;
+	}
+
+	public static void setBrowserName(String browserName) {
+		DriverManager.browserName = browserName;
 	}
 
 }
